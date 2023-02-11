b74c572a6d1bff923660e13efef73c83f63a8b76
Junhyeong_Choi <junhyung9571@naver.com>
2023-02-11 22:26:41 +0900

    Feat: BVH Construction

--- Assets/01. Scripts/BVH.cs
+++ Assets/01. Scripts/BVH.cs
@@ -13,4 +13,23 @@ public class BVH
         this.controlPointsMatrix = matrix;
     }
 
+    public void Construct()
+    {
+
+    }
+
+    public (DVector[,], DVector[,]) Subdivision(DVector[,] parentMatrix)
+    {
+        int row = parentMatrix.GetLength(0);
+        int col = parentMatrix.GetLength(1);
+
+        DVector[,] leftChildMatrix = new DVector[row,col];
+        DVector[,] rightChildMatrix = new DVector[row,col];
+
+        
+
+
+        return (leftChildMatrix, rightChildMatrix);
+    }
+
 }
